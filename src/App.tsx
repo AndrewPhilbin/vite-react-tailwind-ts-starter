149018bd10263ce32e5ece05991a00726a1865c9
Andrew Philbin <45773707+AndrewPhilbin@users.noreply.github.com>
2023-03-24 16:36:31 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main className='bg-slate-800 min-h-screen text-slate-300 flex flex-col items-center'>
+      <article className='pt-8'>
+        <h1 className='text-2xl'>
+          Vite React Tailwind Typescript starter template
+        </h1>
+      </article>
+    </main>
+  )
+}
+
+export default App
